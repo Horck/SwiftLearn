6dea930aa54f7f8fa48993835f2d1342d1c0d5a1
horck <horck@163.com>
2019-02-02 16:05:19 +0800

    up

--- SwiftLeanDemo/Initialization.playground/Contents.swift
+++ SwiftLeanDemo/Initialization.playground/Contents.swift
@@ -2,4 +2,93 @@ import UIKit
 
 var str = "初始化"
 
+// 初始化器
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        temperature = 32.0
+    }
+}
+var f = Fahrenheit()
+print("The default temperature is \(f.temperature)° Fahrenheit")
+// prints "The default temperature is 32.0° Fahrenheit"
+
+// 自定义初始化参数
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+// boilingPointOfWater.temperatureInCelsius is 100.0
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+// freezingPointOfWater.temperatureInCelsius is 0.0
+
+// 形式参数名和实际参数标签
+
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red   = red
+        self.green = green
+        self.blue  = blue
+    }
+    init(white: Double) {
+        red   = white
+        green = white
+        blue  = white
+    }
+}
+
+let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+let halfGray = Color(white: 0.5)
+
+
+// 无实际参数标签的初始化器形式参数
+struct Celsius1 {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+    init(_ celsius: Double) {
+        temperatureInCelsius = celsius
+    }
+}
+let bodyTemperature = Celsius1(37.0)
+
+
+// 在初始化中分配常量属性
+class SurveyQuestion {
+    let text: String
+    var response: String?
+    init(text: String) {
+        self.text = text // 初始化时候给常量赋值
+    }
+    func ask() {
+        print(text)
+    }
+}
+
+let beetsQuestion = SurveyQuestion(text: "How about beets?")
+beetsQuestion.ask()
+// prints "How about beets?"
+beetsQuestion.response = "I also like beets. (But not with cheese.)"
+
+
+// 结构体成员初始化器
+struct Size{
+    var width = 0.0, height = 0.0
+}
+
+// Size 结构体自动接收一个 init(width:heght:) 成员初始化器，你可以使用它来初始化一个新的 Size 实例：
+let twoByTwo = Size(width: 2, height: 2)
+
+
 
